fe28cbdc8c32de64e13b01a0051a5a7b9cbe0c78
Densitty <dennis.brough@yandex.com>
2021-07-12 20:02:08 -0500

    first taste into the world of redux with react

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+// components
+import Navbar from "./components/Navbar";
+import CartContainer from "./components/CartContainer";
+// items
+import cartItems from "./cart-items";
 
+// redux stuff
+
+// store - stores data (something like state)
+import { createStore } from "redux";
+// import { DECREASE, INCREASE } from "./actions";
+import reducer from "./reducer";
+// react-redux - Provider - wraps app , connect - used in components
+import { Provider } from "react-redux";
+
+// initial store
+const initialStore = { cart: cartItems, total: 0, amount: 0 };
+
+/* function reducer(state, action) {
+  console.log({ state, action });
+  if (action.type === DECREASE) {
+    // state.count = state.count - 1 // never mutate state
+    // Object.assign({}, state) is same as {...state}
+    return {
+      ...state, 
+      count: state.count - 1,
+    };
+  }
+
+  if (action.type === INCREASE) {
+    return {
+      ...state,
+      count: state.count + 1,
+    };
+  }
+
+  if (action.type === "RESET") {
+    return {
+      ...state,
+      count: 0,
+    };
+  }
+  // (old) state is what a reducer must return
+  return state;
+} */
+
+const store = createStore(
+  reducer,
+  /* initialStore, */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+// after store is initialized, create a dispatch method (send actions to the store)
+// action (an object) - must have TYPE property to show which kind of action we want to undertake b ased of functionality of our app
+
+// console.log(store.getState());
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Navbar />
+      <CartContainer />
+    </Provider>
   );
 }
 
